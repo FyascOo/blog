b1c26e03f3888a1f4d724f446ff6bfef27c8f86e
FyascOo <alan.choufa@gmail.com>
2025-10-01 22:57:23 +0200

    css header

--- libs/src/lib/ui/header.component.ts
+++ libs/src/lib/ui/header.component.ts
@@ -1,40 +1,24 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ui-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink],
   template: `
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-      <div
-        class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
-      >
-        <a
-          [routerLink]="['/']"
-          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-          >Chouf'academy</a
-        >
-
-        <div
-          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
-        >
-          <a
-            [routerLink]="['/tips']"
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-          >
-            Tips
+    <nav class=" border-gray-200 bg-gray-900">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a [routerLink]="['/']" class="self-center text-2xl font-semibold whitespace-nowrap text-white">
+          Chouf'academy
+        </a>
+        <div class="font-medium flex p-0 rounded-lg flex-row space-x-8 rtl:space-x-reverse mt-0 border-0 ">
+          <a [routerLink]="['/tips']" class="block py-2 px-3 border-0 p-0 rounded hover:bg-gray-700 text-white">Tips</a>
+          <a [routerLink]="['/posts']" class="block py-2 px-3 border-0 p-0 rounded hover:bg-gray-700 text-white">
+            Posts
           </a>
-          <a
-            [routerLink]="['/posts']"
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >Posts</a
-          >
-          <a
-            [routerLink]="['/portfolio']"
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-            >Portfolio</a
-          >
+          <a [routerLink]="['/portfolio']" class="block py-2 px-3 border-0 p-0 rounded hover:bg-gray-700 text-white">
+            Portfolio
+          </a>
         </div>
       </div>
     </nav>
